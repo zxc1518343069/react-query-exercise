0c0943cb91a59b328f51f53562147c89b9831898
achen <1518343069@qq.com>
2022-07-23 17:23:06 +0800

    update:  fix mistake

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const getSuper = () => {
   return axios.get("http://localhost:4000/superheroes");
@@ -7,12 +8,8 @@ const getSuper = () => {
 
 export const RQSuperHeroesPage = () => {
   // 更多状态请参考 https://tanstack.com/query/v4/docs/reference/useQuery
-  const { isLoading, data, isError, error, isFetching } = useQuery(
-    ["super"],
-    getSuper
-  );
+  const { isLoading, data, isError, error } = useQuery(["super"], getSuper);
 
-  console.log(isLoading, isFetching);
   // isLoading 和 isFetch 的区别在于  isLoading  更多是在没有缓存的条件下 第一次请求数据做的。
   // isFetching 表现在请求，即 只要请求就有状态改变。
   if (isLoading) {
@@ -28,7 +25,11 @@ export const RQSuperHeroesPage = () => {
     <>
       <h2>Super Heroes Page</h2>
       {data?.data.map((hero) => {
-        return <div key={hero.id}>{hero.name}</div>;
+        return (
+          <div key={hero.id}>
+            <Link to={`/superheroe/${hero.id}`}>{hero.name}</Link>
+          </div>
+        );
       })}
     </>
   );
