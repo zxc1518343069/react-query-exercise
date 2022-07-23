944a78a67572049f5f74c864a092cc63b39067b8
achen <1518343069@qq.com>
2022-07-23 17:09:11 +0800

    feat: 使用useQuery 根据Id 请求

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
 import { SuperHeroesPage } from "./components/SuperHeroes.page";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import QueryById from "./components/QueryById";
 
 const client = new QueryClient();
 function App() {
@@ -27,6 +28,7 @@ function App() {
           </nav>
           <Routes>
             <Route path="/super-heroes" element={<SuperHeroesPage />} />
+            <Route path="/superheroe/:id" element={<QueryById />} />
             <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
             <Route path="/" element={<HomePage />} />
           </Routes>
